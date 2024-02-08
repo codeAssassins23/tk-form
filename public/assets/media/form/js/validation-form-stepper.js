7d4e9f4dc4011f62d06f1e5d6120f10ce6464528
Christian Tong Cruz <christian.tong@tkambio.us>
2024-02-08 15:17:18 -0500

    register-form-stepp-1

--- public/assets/media/form/js/validation-form-stepper.js
+++ public/assets/media/form/js/validation-form-stepper.js
@@ -4,58 +4,137 @@ const form = document.getElementById('kt_sign_up_form');
 // Init validartion 1
 let validatorStep1 = FormValidation.formValidation(form, {
   fields: {
-    text_email: {
+    corporateName: {
       validators: {
-        emailAddress: {
-          message: 'El contenido no es válidos',
-        },
         notEmpty: {
           message: 'Email es requerido',
         },
       },
     },
-    text_name: {
+    tradeNameOfDBA: {
       validators: {
         notEmpty: {
           message: 'Nombrey apellido es requerido',
         },
       },
     },
-    text_enterprice: {
+    state: {
       validators: {
         notEmpty: {
           message: 'Empresa es requerida',
         },
       },
     },
-    select_country: {
+    city: {
       validators: {
         notEmpty: {
           message: 'País es requerido',
         },
       },
     },
-    text_phone: {
+    postalCode: {
       validators: {
         notEmpty: {
           message: 'Telefono es requerido',
         },
       },
     },
-  },
-  plugins: {
-    trigger: new FormValidation.plugins.Trigger(),
-    bootstrap: new FormValidation.plugins.Bootstrap5({
-      rowSelector: '.fv-row',
-      eleInvalidClass: '',
-      eleValidClass: '',
-    }),
+    address: {
+      validators: {
+        notEmpty: {
+          message: 'Empresa es requerida',
+        },
+      },
+    },
+    emailInfomation: {
+      validators: {
+        notEmpty: {
+          message: 'País es requerido',
+        },
+        emailAddress: {
+          message: 'El contenido no es válidos',
+        },
+      },
+    },
+    phoneInformation: {
+      validators: {
+        notEmpty: {
+          message: 'Telefono es requerido',
+        },
+      },
+    },
+    website: {
+      validators: {
+        notEmpty: {
+          message: 'Empresa es requerida',
+        },
+      },
+    },
+    TaxIdentificationNumber: {
+      validators: {
+        notEmpty: {
+          message: 'País es requerido',
+        },
+      },
+    },
+    industry: {
+      validators: {
+        notEmpty: {
+          message: 'Telefono es requerido',
+        },
+      },
+    },
+    natureOfBusiness: {
+      validators: {
+        notEmpty: {
+          message: 'Telefono es requerido',
+        },
+      },
+    },
+    DateOfIncorporation: {
+      validators: {
+        notEmpty: {
+          message: 'Empresa es requerida',
+        },
+      },
+    },
+    typeOfBusiness: {
+      validators: {
+        notEmpty: {
+          message: 'País es requerido',
+        },
+      },
+      isTheApplicant: {
+        validators: {
+          notEmpty: {
+            message: 'Telefono es requerido',
+          },
+        },
+      },
+    },
+    plugins: {
+      trigger: new FormValidation.plugins.Trigger(),
+      bootstrap: new FormValidation.plugins.Bootstrap5({
+        rowSelector: '.fv-row',
+        eleInvalidClass: '',
+        eleValidClass: '',
+      }),
+    },
   },
 });
 
-const submitButton = document.getElementById('kt_sign_up_submit');
+const submitButton = document.getElementsByClassName('stepp_1');
 
-submitButton.addEventListener('click', async function () {});
+submitButton.addEventListener('click', async function (e) {
+  // Prevent default button action
+  e.preventDefault();
+  // Validate the form based on the current step
+  let isValidStep1 = await validateStep1();
+
+  if (isValidStep1) {
+    console.log('Work');
+  }
+});
 
 // Function to validate Step 1
 const validateStep1 = async () => {
