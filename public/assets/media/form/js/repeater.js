27d445067fb53569931ee8fe1c8e12943e5990a0
Christian Tong Cruz <christian.tong@tkambio.us>
2024-02-13 17:16:08 -0500

    register-form

--- public/assets/media/form/js/repeater.js
+++ public/assets/media/form/js/repeater.js
@@ -1,20 +1,21 @@
+/* eslint-disable prettier/prettier */
 let index = 0;
 $('#kt_docs_repeater_basic').repeater({
   initEmpty: false,
 
   defaultValues: {
-      'text-input': 'foo'
+    'text-input': 'foo',
   },
 
   show: function () {
-      if(index < 1){
-        index = ++index;
-        $(this).slideDown();
-      }
+    if (index < 1) {
+      index = ++index;
+      $(this).slideDown();
+    }
   },
 
   hide: function (deleteElement) {
     index = --index;
-      $(this).slideUp(deleteElement);
-  }
+    $(this).slideUp(deleteElement);
+  },
 });
