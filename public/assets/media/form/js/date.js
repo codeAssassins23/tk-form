fb9f6d2d1b3c9c2052ac3721e87369ba74766fce
Christian Tong Cruz <christian.tong@tkambio.us>
2024-02-12 21:43:52 -0500

    register-form

--- public/assets/media/form/js/date.js
+++ public/assets/media/form/js/date.js
@@ -1,17 +1,4 @@
-new tempusDominus.TempusDominus(
-  document.getElementById('kt_td_picker_date_only'),
-  {
-    display: {
-      viewMode: 'calendar',
-      components: {
-        decades: true,
-        year: true,
-        month: true,
-        date: true,
-        hours: false,
-        minutes: false,
-        seconds: false,
-      },
-    },
-  },
-);
+/* eslint-disable prettier/prettier */
+
+$('#kt_datepicker_1').flatpickr();
+$('#kt_datepicker_2').flatpickr();
---
+++ public/assets/media/form/js/repeater.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+$('#kt_docs_repeater_basic').repeater({
+  initEmpty: false,
+
+  defaultValues: {
+    'text-input': 'foo',
+  },
+
+  show: function () {
+    $(this).slideDown();
+  },
+
+  hide: function (deleteElement) {
+    $(this).slideUp(deleteElement);
+  },
+});
