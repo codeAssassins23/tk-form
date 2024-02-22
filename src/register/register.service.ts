acc551ca8b01f90e5fb0f3e74fb5731ae05c30e7
codeAssassins23 <bravovictorhugo11@gmail.com>
2024-02-22 09:41:17 -0500

    register-form

--- src/register/register.service.ts
+++ src/register/register.service.ts
@@ -34,8 +34,8 @@ export class RegisterService {
 
     // Enviar correo de notificación a la dirección específica después de guardar el registro
     const notificationEmail =
-      'customer-service@tkambio.us'; /* 'bravovictorhugo11@gmail.com' */ // Definir el correo al que notificar
-    await this.mailService.sendRegistrationNotification(
+      'bravovictorhugo11@gmail.com'; /* 'bravovictorhugo11@gmail.com' */ // Definir el correo al que notificar
+    /* 'customer-service@tkambio.us' */ await this.mailService.sendRegistrationNotification(
       save,
       notificationEmail,
     );
@@ -195,7 +195,8 @@ export class RegisterService {
       register.status = '1';
       const save = await this.registerRepository.save(register);
       // Enviar correo de notificación a la dirección específica después de guardar el registro
-      const notificationEmail = 'customer-service@tkambio.us'; // Definir el correo al que notificar
+      const notificationEmail =
+        'bravovictorhugo11@gmail.com'; /* 'customer-service@tkambio.us' */ // Definir el correo al que notificar
       await this.mailService.sendFinalRegistrationNotification(
         save,
         notificationEmail,
